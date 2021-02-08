671fb7ec5975b13da0c5257783fa2f64111e40c1
takion9538 <hershey9538@gmail.com>
2021-02-08 22:21:17 +0900

    message use

--- app/src/main/java/com/budi/myapplication/MainActivity.kt
+++ app/src/main/java/com/budi/myapplication/MainActivity.kt
@@ -3,66 +3,183 @@ package com.budi.myapplication
 import android.animation.ObjectAnimator
 import android.graphics.Point
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.util.Log
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
+import android.view.MotionEvent
+import android.view.View.*
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // 각 버튼의 상태 저장
+    private var Btn1Down = false
+    private var Btn2Down = false
+
+    // onCreate 함수 선언
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 뷰의 선언
-        val imageView1: ImageView = findViewById(R.id.dot1)
+        // 네비게이션 바와 상태창 숨기기
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        or SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        or SYSTEM_UI_FLAG_IMMERSIVE)
 
-        // 버튼 선언
-        val Btn1 = findViewById<Button>(R.id.Btn1)
-        val Btn2 = findViewById<Button>(R.id.Btn2)
+        // 버튼 1 선언 및 버튼터치 리스너 지정
+        Btn1!!.setOnTouchListener(onBtn1TouchListener)
 
-        // 뷰의 좌표값 받아오기
-        fun View.getLocationOnScreen(): Point {
-            val location = IntArray(2)
-            this.getLocationOnScreen(location)
-            return Point(location[0], location[1])
-        }
+        // 버튼 2 선언 및 버튼터치 리스너 지정
+        Btn2!!.setOnTouchListener(onBtn2TouchListener)
+    }
 
-        // absX 와 absY 에 뷰의 현재 절대좌표값을 저장
-        val location = imageView1.getLocationOnScreen()
-        val absX: Float = location.x.toFloat()
-        val absY: Float = location.y.toFloat()
+    // onBtnDown 작업을 처리하는 쓰레드를 생성
+    private fun onBtnDown() {
+        val kThread = TouchThread()
+        kThread.start()
+    }
 
-        // 뷰의 좌표값을 로그에 출력
-        Log.d("뷰  좌표", "${absX}, ${absY}");
+    // 왼쪽으로 이동하는 동안의 핸들러
+    private val touch1Handler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
 
-        // 이미지뷰1 의 Width, Height 를 받아옴
-        val parentWidth = (imageView1.parent as ViewGroup).width
-        val parentHeight = (imageView1.parent as ViewGroup).height
-
-        // 이미지뷰1 의 x 와 y 위치값을 중앙으로 설정
-        imageView1.x = absX - parentWidth
-        imageView1.y = absY - parentHeight
-
-        // 버튼1 이 클릭되었을 때
-        Btn1.setOnClickListener() {
-            // 이미지뷰1 의 위치를 이동
-            ObjectAnimator.ofFloat(imageView1, "translationX", imageView1.x - 100f).apply {
-                duration = 100
+            // Handler 가 작동할 때 (Btn1 버튼을 키다운 할 때) 마다 도트의 위치를 왼쪽으로 100float 만큼 이동
+            ObjectAnimator.ofFloat(dot1, "translationX", dot1.x - 70f).apply {
+                duration = 200
                 start()
             }
         }
+    }
 
-        // 버튼2 가 클릭되었을 때
-        Btn2.setOnClickListener() {
-            // 이미지뷰1 의 위치를 이동
-            ObjectAnimator.ofFloat(imageView1, "translationX", imageView1.x + 100f).apply {
-                duration = 100
+    // 오른쪽으로 이동하는 동안의 핸들러
+    private val touch2Handler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+
+            // Handler 가 작동할 때 (Btn2 버튼을 키다운 할 때) 마다 도트의 위치를 오른쪽으로 100float 만큼 이동
+            ObjectAnimator.ofFloat(dot1, "translationX", dot1.x + 70f).apply {
+                duration = 200
                 start()
             }
+        }
+    }
+
+    // '터치쓰레드' 클래스를 생성
+    private inner class TouchThread : Thread() {
+
+        // run 함수 호출
+        override fun run() {
+
+            // 뷰의 크기 가져오기
+            val dot1Size = dot1.width
+
+            // 화면의 크기 가져오기
+            var display = windowManager.defaultDisplay
+            val size = Point()
+            display.getRealSize(size)
+            val dpwidth = size.x
+
+            // 버튼 1 이 클릭된 상태라면
+            while (Btn1Down) {
+                
+                // 캐릭터가 화면의 왼쪽 끝에 닿기 전까지
+                if (dot1.x >= 0) {
+                    
+                    // 좌측으로 이동하는 함수를 호출
+                    touch1Handler.sendEmptyMessage(1)
+                    sleep(100L)
+
+                    try {
+                        // 이미지의 리소스를 변경 (좌측으로 걷기)
+                        dot1.setImageResource(R.drawable.dot_left)
+                        sleep(100L)
+                        dot1.setImageResource(R.drawable.dot_left_2)
+                        
+                    } 
+                    catch (e: Exception) {
+                        println("error")
+                    }
+                }
+            }
+            
+            // 버튼 2 가 클릭된 상태라면
+            while (Btn2Down) {
+                
+                // 캐릭터가 화면의 오른쪽 끝에 닿기 전까지
+                if (dot1.x + dot1Size <= dpwidth.toFloat()) {
+                    
+                    // 우측으로 이동하는 함수를 호출
+                    touch2Handler.sendEmptyMessage(1)
+                    sleep(100L)
+
+                    try {
+                        // 이미지의 리소스를 변경 (우측으로 걷기)
+                        dot1.setImageResource(R.drawable.dot_right)
+                        sleep(100L)
+                        dot1.setImageResource(R.drawable.dot_right_2)
+                    } 
+                    
+                    catch (e: Exception) {
+                        println("error")
+                    }
+                }
+            }
+            sleep(100L)
+            return dot1.setImageResource(R.drawable.dot)
+            super.run()
+        }
+    }
+
+    // 버튼 1 이 클릭된 상태인지 검증하는 함수
+    private val onBtn1TouchListener = OnTouchListener { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Btn1Down = true
+                onBtnDown()
+            }
+            MotionEvent.ACTION_UP -> Btn1Down = false
+            else -> {
+            }
+        }
+        false
+    }
+
+    // 버튼 2 가 클릭된 상태인지 검증하는 함수
+    private val onBtn2TouchListener = OnTouchListener { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Btn2Down = true
+                onBtnDown()
+            }
+            MotionEvent.ACTION_UP -> Btn2Down = false
+            else -> {
+            }
+        }
+        false
+    }
+
+    // 화면이 터치되면 화면을 전체화면으로 변경하는 함수
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        // 만약 터치 다운 이벤트가 발생했을 때 다음 값을 리턴
+        return when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+                // 네비게이션 바와 상태창 숨기기
+                this.getWindow().getDecorView().setSystemUiVisibility(
+                        SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                or SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                or SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                or SYSTEM_UI_FLAG_IMMERSIVE)
+                true
+            }
+            else -> return super.onTouchEvent(event)
         }
     }
 }
