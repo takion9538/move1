5ddb816696f31e8e3f02ecc8366b7e2e6ac3b291
takion9538 <hershey9538@gmail.com>
2021-02-12 16:55:02 +0900

    message use

--- app/src/main/java/com/budi/myapplication/MainActivity.kt
+++ app/src/main/java/com/budi/myapplication/MainActivity.kt
@@ -3,18 +3,21 @@ package com.budi.myapplication
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Point
-import android.media.AudioAttributes
-import android.media.AudioManager
-import android.media.MediaPlayer
-import android.media.SoundPool
+import android.media.*
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.provider.MediaStore
 import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import android.view.View.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Thread.sleep
 
@@ -53,10 +56,22 @@ class MainActivity : AppCompatActivity() {
         // 버튼 2 선언 및 버튼터치 리스너 지정
         Btn2.setOnTouchListener(onBtn2TouchListener)
 
+        btnAction.setOnClickListener() {
+            Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
+        
+        // 버섯 핸들러를 호출
+        mushroomHandler.sendEmptyMessage(1)
+    }
+
+    override fun onStart() {
         // 배경음악 설정 -> 모든 어플리케이션 콘텐츠에서 허용
         val bgm1 = MediaPlayer.create(applicationContext, R.raw.restinforest)
-        bgm1.isLooping = true // 루프 활성화
-        bgm1.start() // 배경음악 실행
+
+//        bgm1.start()
+        bgm1.isLooping = true
+
+        super.onStart()
     }
 
     // onBtnDown 작업을 처리하는 쓰레드를 생성
@@ -71,7 +86,7 @@ class MainActivity : AppCompatActivity() {
 
             // Handler 가 작동할 때 (Btn1 버튼을 키다운 할 때) 마다 도트의 위치를 왼쪽으로 100float 만큼 이동
             ObjectAnimator.ofFloat(dot1, "translationX", dot1.x - 80f).apply {
-                duration = 200
+                duration = 500
                 start()
             }
         }
@@ -83,8 +98,42 @@ class MainActivity : AppCompatActivity() {
 
             // Handler 가 작동할 때 (Btn2 버튼을 키다운 할 때) 마다 도트의 위치를 오른쪽으로 100float 만큼 이동
             ObjectAnimator.ofFloat(dot1, "translationX", dot1.x + 80f).apply {
-                duration = 200
+                duration = 500
                 start()
+            }
+        }
+    }
+
+    // 슬라임 움직임 핸들러 테스트 1
+    private val slimeHandler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+
+            ObjectAnimator.ofFloat(slime, "translationX", dot1.x -40f).apply {
+                duration = 500
+                start()
+            }
+        }
+    }
+
+    // 슬라임 움직임 테스트 핸들러 2
+    private val slime2Handler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+
+            ObjectAnimator.ofFloat(slime, "translationX", dot1.x + dot1.width + 40f).apply {
+                duration = 500
+                start()
+            }
+        }
+    }
+
+    // 현재 dot 1 이미지 뷰의 좌표를 불러와 버섯의 범위 안에 있을 경우 버튼의 상태를 visible 로 변경
+    private val mushroomHandler : Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            if (dot1.x > (mushroom.x - dot1.width - 100f) && dot1.x < (mushroom.x + mushroom.width)) {
+                btnAction.visibility = Button.VISIBLE
+            } else {
+                btnAction.visibility = Button.INVISIBLE
             }
         }
     }
@@ -111,24 +160,27 @@ class MainActivity : AppCompatActivity() {
             while (Btn1Down) {
                 
                 // 캐릭터가 화면의 왼쪽 끝에 닿기 전까지
-                if (dot1.x >= 0) {
+                if (dot1.x > 0) {
+
+                    // 버섯 핸들러를 호출
+                    mushroomHandler.sendEmptyMessage(1)
 
                     // 좌측으로 이동하는 함수를 호출
                     touch1Handler.sendEmptyMessage(1)
                     sleep(100L)
 
                     try {
-                        // 걸음소리를 호출
-                        soundPool.play(footprint, 1.0f, 1.0f, 1, 0, 0.7f)
-
                         // 이미지의 리소스를 변경 (좌측으로 걷기)
                         dot1.setImageResource(R.drawable.witchtest)
-                        sleep(100L)
+                        sleep(200L)
+                        // 걸음소리를 호출
+                        soundPool.play(footprint, 1.0f, 1.0f, 1, 0, 1f)
+
                         dot1.setImageResource(R.drawable.witchtest)
+                        sleep(200L)
+                        slime2Handler.sendEmptyMessage(1)
                     } 
                     catch (e: Exception) {
-                        println("error")
-                        sleep(100L)
                     }
                 }
             }
@@ -138,26 +190,29 @@ class MainActivity : AppCompatActivity() {
                 
                 // 캐릭터가 화면의 오른쪽 끝에 닿기 전까지
                 if (dot1.x + dot1Size <= dpwidth.toFloat()) {
+
+                    // 버섯 핸들러를 호출
+                    mushroomHandler.sendEmptyMessage(1)
                     
                     // 우측으로 이동하는 함수를 호출
                     touch2Handler.sendEmptyMessage(1)
                     sleep(100L)
 
                     try {
-                        // 걸음소리를 호출
-                        soundPool.play(footprint, 1.0f, 1.0f, 1, 0, 0.7f)
-
                         // 이미지의 리소스를 변경 (우측으로 걷기)
                         dot1.setImageResource(R.drawable.dot_right)
-                        sleep(100L)
+                        sleep(200L)
+                        // 걸음소리를 호출
+                        soundPool.play(footprint, 1.0f, 1.0f, 1, 0, 1f)
+
                         dot1.setImageResource(R.drawable.dot_right_2)
+                        sleep(200L)
+                        slimeHandler.sendEmptyMessage(1)
                     }
                     catch (e: Exception) {
-                        sleep(100L)
                     }
                 }
             }
-            sleep(100L)
             return dot1.setImageResource(R.drawable.witchdot1)
             super.run()
         }
@@ -170,7 +225,9 @@ class MainActivity : AppCompatActivity() {
                 Btn1Down = true
                 onBtnDown()
             }
-            MotionEvent.ACTION_UP -> Btn1Down = false
+            MotionEvent.ACTION_UP -> {
+                Btn1Down = false
+            }
             else -> {
             }
         }
@@ -184,7 +241,9 @@ class MainActivity : AppCompatActivity() {
                 Btn2Down = true
                 onBtnDown()
             }
-            MotionEvent.ACTION_UP -> Btn2Down = false
+            MotionEvent.ACTION_UP -> {
+                Btn2Down = false
+            }
             else -> {
             }
         }
